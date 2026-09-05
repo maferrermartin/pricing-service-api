386aaa9b1748e63eec12d01f22e41442dac0066b
Miguel Angel Ferrer <45531622+maferrermartin@users.noreply.github.com>
2026-09-05 18:53:26 +0200

    Spring initializr

---
+++ src/main/java/com/github/maferrermartin/MaferrermartinApplication.java
@@ -0,0 +1,13 @@
+package com.github.maferrermartin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaferrermartinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaferrermartinApplication.class, args);
+	}
+
+}
